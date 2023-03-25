422cc06325d63544387ea1e3a81beeb7502b33d9
tulsisatasiya <115995210+tulsisatasiya@users.noreply.github.com>
2023-03-25 11:04:31 +0530

    Add files via upload

---
+++ p2.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+main(){
+
+	 float x,y; 
+
+	 printf("Enter the value of x:");	 
+	 scanf("%f",&x);
+	 printf("Enter the value of y:");	 
+	 scanf("%f",&y);
+	 float w;
+	 w =(x-y)*(x-y);
+  	 printf("answer is:%f",w);
+}
---
+++ p6.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+main(){
+
+	 float x,y,z;	 
+
+	 printf("Enter the value of x:");	 
+	 scanf("%f",&x);
+	 printf("Enter the value of y:");	 
+	 scanf("%f",&y);
+	 printf("Enter the value of z:");	 
+	 scanf("%f",&z);
+	 float w;
+	 w =(x-y-z)*3;
+  	 printf("answer is:%f",w);
+}
